9d850696a1696d1a92a238e1d29f3b4282e730c4
kuter <contact@devktr.pl>
2020-06-03 21:10:12 +0200

    WIP

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './users/login/login.component';
+import { LogoutComponent } from './users/logout/logout.component';
+import { RegisterComponent } from './users/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'logout', component: LogoutComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
