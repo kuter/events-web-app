603fe5bb90750c423b676b68cc82bf839c97217d
kuter <contact@devktr.pl>
2020-06-06 11:39:10 +0200

    :heavy_plus_sign: add simplejwt and corsheaders

--- urls.py
+++ urls.py
@@ -3,7 +3,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import routers
+
+from events import api
+
+router = routers.DefaultRouter()
+router.register(r'events', api.EventViewSet)
+
+
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('participants/', include('participants.urls')),
     path('admin/', admin.site.urls),
     path('', include('events.urls', namespace='events')),
