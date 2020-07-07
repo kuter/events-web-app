d7ab84e50e9aff1ef452f63d4eb48a648bfefc35
kuter <contact@devktr.pl>
2020-07-07 22:29:51 +0200

    :card_file_box: improve performance

--- events/managers.py
+++ events/managers.py
@@ -7,7 +7,9 @@ class EventManager(models.Manager):
     """Custom manager for Event objects."""
 
     def incoming(self):  # noqa: D102
-        return super().get_queryset().filter(
+        return super().get_queryset().prefetch_related(
+            'eventparticipant_set'
+        ).filter(
             date__gte=datetime.date.today(),
         ).order_by(
             'date',
