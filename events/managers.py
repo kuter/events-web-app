77ee6ee0fbdfd29e3e4dbc8b9685099844d21a98
kuter <contact@devktr.pl>
2020-07-07 22:45:21 +0200

    :lipstick: fix codestyle

--- events/managers.py
+++ events/managers.py
@@ -8,7 +8,7 @@ class EventManager(models.Manager):
 
     def incoming(self):  # noqa: D102
         return super().get_queryset().prefetch_related(
-            'eventparticipant_set'
+            'eventparticipant_set',
         ).filter(
             date__gte=datetime.date.today(),
         ).order_by(
